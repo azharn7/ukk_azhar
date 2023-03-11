8a94a64cd9f1d58c704c0ea16fe10b3db1710df5
azharn7 <127082989+azharn7@users.noreply.github.com>
2023-03-11 08:28:56 +0700

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+<footer class="fixed-bottom" style="background-color: lightblue">
+  <div class="container">
+    <p class="text-center">UKK RPL 2023 | Azhar | SMK MAHARDIKA</p>
+  </div>
+
+</footer>
+
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
