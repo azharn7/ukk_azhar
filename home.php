9bac8d26c97cf068773954c02ee01a14a3276f25
azharn7 <127082989+azharn7@users.noreply.github.com>
2023-03-11 10:24:30 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,34 @@
+<div class="container"> 
+    <h4 class="text-center mt-3">Uji Kompetensi Keahlian <br>Rekayasa Perangkat Lunak (RPL) <br> Tahun 2022/2023</h4>
+    <hr>
+    <div class="row mt-3">
+    <div class="col-md-8">
+        <div class="card">
+        <div class="card-header" style="background-color: lightblue;"><b>INFORMASI</b></div>
+        <div class="card-body">
+            Aplikasi Pelaporan Pengaduan Masyarakat ini dibuat untuk memenuhi Tugas Uji Kompetensi Keahlian RPL Tahun 2023<br><br>
+            Petunjuk Penggunaan Aplikasi :
+            <br>1.Daftar Akun terlebih dahulu, Jika belum mempunyai akun, sesudah mempunyai akun Klik Login
+            <br>2.Ketika sudah Login, Masyarakat bisa mengisi Form Pengaduan, Isi Form Pengaduan lalu kirim
+            <br>3.Masyarakat tinggal menunggu tanggapan dari Petugas
+            <br>4.Jika sudah di Tanggapi oleh petugas akan menampilkan <u>"Lihat Tanggapan"</u> pada list "STATUS"
+            <br>5.Klik <u>"Lihat Tanggapan"</u> untuk melihat tanggapan dari Petugas
+        
+        </div>
+        <div class="card-footer" style="background-color: lightblue;"></div>
+        </div>
+    </div>
+    <div class="col-md-4">
+    <div class="card">
+        <div class="card-header" style="background-color: lightblue;"><b>KONTAK</b></div>
+        <div class="card-body">
+           Dikembangkan oleh <br>
+           Nama  : Azhar Nugraha Permadi<br>
+           Kelas : XII RPL 2<br>
+           Instagram: azharnugraha._<br>
+</div>
+        <div class="card-footer" style="background-color: lightblue;"></div>
+        </div>
+    </div>
+    </div>
+</div>
